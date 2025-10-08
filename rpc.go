7551bcc15f2d6e24620346314ca30810d49fdf1d
Pouya Vedadiyan <vedadiyan@gmail.com>
2025-10-08 18:17:15 +0800

    rpc

--- rpc.go
+++ rpc.go
@@ -1,11 +1,13 @@
 package protolizer
 
 import (
+	"context"
 	"net/url"
 )
 
 type Header map[string][]string
 type Options map[string]string
+type Handler[I Reflected, O Reflected] func(*Request[I]) (*Response[O], error)
 
 type Request[T Reflected] struct {
 	Headers    Header
@@ -21,4 +23,17 @@ type Response[T Reflected] struct {
 	Data       T
 	StatusCode int
 	Options    Options
+}
+
+type RpcOption struct {
+	options any
+}
+
+func UnwrapRpcOptions[T any](rpcOption *RpcOption) (*T, bool) {
+	out, ok := rpcOption.options.(T)
+	return &out, ok
+}
+
+type Server interface {
+	Handler(RpcOption, func(context.Context, *Request[Reflected]) (*Response[Reflected], error))
 }
