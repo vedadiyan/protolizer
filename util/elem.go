fc369cb8de07a30476f339587951b66525d9d179
Pouya Vedadiyan <vedadiyan@gmail.com>
2025-11-06 17:04:43 +0800

    pointer

--- util/elem.go
+++ util/elem.go
@@ -1,10 +1,22 @@
 package util
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func GetElemenType(t reflect.Type) reflect.Type {
 	for t.Kind() == reflect.Ptr || t.Kind() == reflect.Slice || t.Kind() == reflect.Array || t.Kind() == reflect.Map {
 		t = t.Elem()
 	}
 	return t
+}
+
+func Value(v reflect.Value) reflect.Value {
+	for v.Kind() == reflect.Pointer {
+		if v.IsNil() {
+			v.Set(reflect.New(v.Type().Elem()))
+		}
+		v = v.Elem()
+	}
+	return v
 }
